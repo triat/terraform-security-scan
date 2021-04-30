1d97206189bc94563d54c817b9a07f75c9e43eaa
Max N. Boyarov <1036881+zotrix@users.noreply.github.com>
2021-04-30 14:09:15 +0300

    fix versioned installation tfsec (#35)
    Versioned installation should be without updating deps (-u) flag.
    Update deps could cause build fail.
    
    Co-authored-by: Max N. Boyarov <zotrix@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,7 +23,7 @@ fi
 
 # grab tfsec from GitHub (taken from README.md)
 if [[ -n "$INPUT_TFSEC_VERSION" ]]; then
-  env GO111MODULE=on go get -u github.com/tfsec/tfsec/cmd/tfsec@"${INPUT_TFSEC_VERSION}"
+  env GO111MODULE=on go install github.com/tfsec/tfsec/cmd/tfsec@"${INPUT_TFSEC_VERSION}"
 else
   env GO111MODULE=on go get -u github.com/tfsec/tfsec/cmd/tfsec
 fi
